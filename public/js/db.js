3684942e43e53ded20bc1fa561d537e6966e5005
Joaquín <jncaceres@uc.cl>
2022-05-07 13:33:21 -0400

    notifications

--- public/js/db.js
+++ public/js/db.js
@@ -1,3 +1,4 @@
+const tokens = []
 //offline data
 db.enablePersistence()
     .catch(err => {
@@ -26,6 +27,24 @@ db.collection('news').onSnapshot(snapshot => {
 })
 
 
+// real-time listener
+db.collection('tokens').onSnapshot(snapshot => {
+    snapshot.docChanges().forEach(change => {
+        // console.log(change, change.doc.data(), change.doc.id);
+        if (change.type === 'added') {
+            // add the document data to the web page
+            tokens.push(change.doc.data().token)
+        }
+        if (change.type === 'removed') {
+            // remove the document data from the web page
+            const index = tokens.indexOf(change.doc.data().token)
+            if (index !== -1) {
+                tokens.splice(index, 1)
+            }
+        }
+    })
+})
+
 // add news
 const form = document.querySelector('form');
 form.addEventListener('submit', evt => {
@@ -38,6 +57,28 @@ form.addEventListener('submit', evt => {
 
     db.collection('news').add(news)
         .catch(err => console.log(err));
+    
+    const bodyToSend = {
+        notification: {
+            title: form.title.value,
+            body: form.body.value,
+            icon: '/img/icons/icon-128x128.png'
+        },
+        android:{
+            priority:"normal"
+          },
+        registration_ids: tokens
+    }
+    
+    fetch('https://fcm.googleapis.com/fcm/send', {
+        method: 'POST',
+        headers: {
+            Authorization: 'key=AAAAgkVZn9Y:APA91bE9ipGA3bzFqPzqzA0xK7q2zbJ6BKhkPjVDvyz_Ccualva7eJmmj4et_HbcZUuFRHMGhfBPnOiMyhHNDQgXoX8217owT2BiQpePB_feDkwDCJKAbsheB6Lirw436QHX3eX_vfAS',
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(bodyToSend)
+
+    }).catch(() => console.log('error sending notification'))
 
     form.title.value = '';
     form.body.value = '';
